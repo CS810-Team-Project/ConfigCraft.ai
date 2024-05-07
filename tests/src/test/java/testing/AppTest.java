367c868aefca9f356fb2cc996e91aef9b49acfd9
AthenaKiriakoulis <athena.kiriakoulis@gmail.com>
2024-05-06 23:02:20 -0400

    dependency fixes

--- tests/src/test/java/testing/AppTest.java
+++ tests/src/test/java/testing/AppTest.java
@@ -1,11 +1,13 @@
+package testing;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public class AppTest {
 
+    @SuppressWarnings("deprecation")
     @Container
     private final GenericContainer<?> javaContainer = new GenericContainer<>("openjdk:latest")
             .withFileSystemBind(".", "/usr/src/app");
@@ -14,10 +16,15 @@ public class AppTest {
     void TestApp() {
         // Compile the Java file inside the Docker container
         String compileCommand = "javac outputFile.java";
-        String compilationOutput = javaContainer.execInContainer("/bin/sh", "-c", compileCommand).getStdout();
+        String compilationOutput = "";
 
+        try {
+            compilationOutput = javaContainer.execInContainer("/bin/sh", "-c", compileCommand).getStdout();
+        } catch(Exception e) {
+            System.err.println("Failure when compiling");
+        }
         // Assert that there are no compilation errors
         // You might need to parse the compilation output to check for errors
-        // Assertions.assertTrue(compilationOutput.contains("error"), "Compilation failed: " + compilationOutput);
+        Assertions.assertFalse(compilationOutput.contains("error"), "Compilation failed: " + compilationOutput);
     }
 }
--- tests/src/test/java/testing/outputFile.java
+++ tests/src/test/java/testing/outputFile.java
@@ -1,3 +1,5 @@
+package testing;
+
 public class outputFile {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
